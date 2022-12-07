86158c03cb00e804650a3aeffc463e0f7cc185ab
go-compile <97609133+go-compile@users.noreply.github.com>
2022-12-07 23:23:51 +0000

    feat: now, seconds, minutes, hours

--- stamp.go
+++ stamp.go
@@ -1,1 +1,25 @@
 package unixtime
+
+import "time"
+
+type Stamp int64
+
+func Now() Stamp {
+	return Stamp(time.Now().Unix())
+}
+
+func Unix(time int64) Stamp {
+	return Stamp(time)
+}
+
+func (s Stamp) Seconds() int64 {
+	return int64(s)
+}
+
+func (s Stamp) Minutes() int64 {
+	return int64(s / 60)
+}
+
+func (s Stamp) Hours() int64 {
+	return int64(s/60) / 60
+}
